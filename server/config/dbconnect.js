8784b7a1383a37f0efbdac0e742cafb1c5582704
Anh Tú <146130167+DevBE2004@users.noreply.github.com>
2024-11-28 11:33:11 +0700

    Update dbconnect.js

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -2,14 +2,13 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    // await mongoose
-    // .connect(process.env.MONGODB_URL, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // })
-    await mongoose.connect(
-      "mongodb://127.0.0.1:27017/" + process.env.MONGODB_NAME
-    );
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // await mongoose.connect(
+    //   "mongodb://127.0.0.1:27017/" + process.env.MONGODB_NAME
+    // );
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
